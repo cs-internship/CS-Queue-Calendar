304ddbc3d636a1e4234de6791fca944b99137316
Ali Sadeghi <ali909392@gmail.com>
2025-01-16 17:52:40 +0330

    Add fancy theme transition, improve calendar size, ready to deploy

--- cs-queue-calendar/src/FloatButtonSection.jsx
+++ cs-queue-calendar/src/FloatButtonSection.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FloatButton, Tooltip } from "antd";
 import { ThemeContext } from "./ThemeContext";
 import {
@@ -19,72 +19,102 @@ const FloatButtonSection = () => {
         window.open(link, "_blank");
     };
 
+    const [showTransition, setShowTransition] = useState(false);
+
+    const handleChangeTheme = () => {
+        setShowTransition(true);
+
+        const timer1 = setTimeout(() => {
+            setShowTransition(false);
+        }, 1200);
+
+        const timer2 = setTimeout(() => {
+            toggleTheme();
+        }, 400);
+
+        return () => {
+            clearTimeout(timer1);
+            clearTimeout(timer2);
+        };
+    };
+
     return (
-        <FloatButton.Group
-            trigger="click"
-            type="primary"
-            icon={<PlusOutlined />}
-            direction="up"
-        >
-            <Tooltip title="CS Internship - Queue Group" placement="right">
-                <FloatButton
-                    icon={<SendOutlined />}
-                    onClick={() => openLink("https://t.me/+5PuhQ2hDIy1lNWRi")}
-                />
-            </Tooltip>
+        <>
+            <FloatButton.Group
+                trigger="click"
+                type="primary"
+                icon={<PlusOutlined />}
+                direction="up"
+            >
+                <Tooltip title="CS Internship - Queue Group" placement="right">
+                    <FloatButton
+                        icon={<SendOutlined />}
+                        onClick={() =>
+                            openLink("https://t.me/+5PuhQ2hDIy1lNWRi")
+                        }
+                    />
+                </Tooltip>
 
-            <Tooltip title="CS Internship - Telegram Channel" placement="right">
-                <FloatButton
-                    icon={<NotificationOutlined />}
-                    onClick={() => openLink("https://t.me/cs_internship")}
-                />
-            </Tooltip>
+                <Tooltip
+                    title="CS Internship - Telegram Channel"
+                    placement="right"
+                >
+                    <FloatButton
+                        icon={<NotificationOutlined />}
+                        onClick={() => openLink("https://t.me/cs_internship")}
+                    />
+                </Tooltip>
 
-            <Tooltip title="CS Internship - GitHub" placement="right">
-                <FloatButton
-                    icon={<GithubOutlined />}
-                    onClick={() => openLink("https://github.com/cs-internship")}
-                />
-            </Tooltip>
+                <Tooltip title="CS Internship - GitHub" placement="right">
+                    <FloatButton
+                        icon={<GithubOutlined />}
+                        onClick={() =>
+                            openLink("https://github.com/cs-internship")
+                        }
+                    />
+                </Tooltip>
 
-            <Tooltip title="CS Internship on LinkedIn" placement="right">
-                <FloatButton
-                    icon={<LinkedinOutlined />}
-                    onClick={() =>
-                        openLink(
-                            "https://www.linkedin.com/company/cs-internship/"
-                        )
-                    }
-                />
-            </Tooltip>
+                <Tooltip title="CS Internship on LinkedIn" placement="right">
+                    <FloatButton
+                        icon={<LinkedinOutlined />}
+                        onClick={() =>
+                            openLink(
+                                "https://www.linkedin.com/company/cs-internship/"
+                            )
+                        }
+                    />
+                </Tooltip>
 
-            <Tooltip title="CS Internship on X (Twitter)" placement="right">
-                <FloatButton
-                    icon={<TwitterOutlined />}
-                    onClick={() =>
-                        openLink("https://x.com/hashtag/cs_internship")
-                    }
-                />
-            </Tooltip>
+                <Tooltip title="CS Internship on X (Twitter)" placement="right">
+                    <FloatButton
+                        icon={<TwitterOutlined />}
+                        onClick={() =>
+                            openLink("https://x.com/hashtag/cs_internship")
+                        }
+                    />
+                </Tooltip>
 
-            <Tooltip title="CS Internship on Virgool" placement="right">
-                <FloatButton
-                    icon={<PaperClipOutlined />}
-                    onClick={() =>
-                        openLink(
-                            "https://virgool.io/cs-internship/cs-internship-k3j2hx4wgvga"
-                        )
-                    }
-                />
-            </Tooltip>
+                <Tooltip title="CS Internship on Virgool" placement="right">
+                    <FloatButton
+                        icon={<PaperClipOutlined />}
+                        onClick={() =>
+                            openLink(
+                                "https://virgool.io/cs-internship/cs-internship-k3j2hx4wgvga"
+                            )
+                        }
+                    />
+                </Tooltip>
 
-            <Tooltip title="Change Theme" placement="right">
-                <FloatButton
-                    icon={<BgColorsOutlined />}
-                    onClick={toggleTheme}
-                />
-            </Tooltip>
-        </FloatButton.Group>
+                <Tooltip title="Change Theme" placement="right">
+                    <FloatButton
+                        icon={<BgColorsOutlined />}
+                        onClick={handleChangeTheme}
+                    />
+                </Tooltip>
+            </FloatButton.Group>
+
+            {showTransition && <div className="fancy-theme-transition"></div>}
+        </>
     );
 };
 
