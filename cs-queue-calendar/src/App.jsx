84c61845f9d6c7f337db93a8c29ba94d9a144e4c
Ali Sadeghi <ali909392@gmail.com>
2025-01-16 15:32:09 +0330

    Add data-theme to project, before add floatBtn w ant design

--- cs-queue-calendar/src/App.jsx
+++ cs-queue-calendar/src/App.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ConfigProvider, theme } from "antd";
 import Header from "./Header";
 import Footer from "./Footer";
 import CSCalendar from "./CSCalendar";
+import { ThemeContext } from "./ThemeContext";
 
 const App = () => {
+    const { theme: currentTheme, toggleTheme } = useContext(ThemeContext);
+
     return (
         <div>
             <Header />
 
+            {/* <button onClick={toggleTheme}>Toggle Theme</button> */}
+
             <ConfigProvider
                 theme={{
-                    algorithm: theme.darkAlgorithm,
+                    algorithm:
+                        currentTheme === "dark"
+                            ? theme.darkAlgorithm
+                            : theme.defaultAlgorithm,
                 }}
             >
                 <CSCalendar />
--- cs-queue-calendar/src/index.jsx
+++ cs-queue-calendar/src/index.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { ThemeProvider } from "./ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
