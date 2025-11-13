d8c6c2ca63ba38ce731ae1dfb5ecc2c8c6a630e6
Ali Sadeghi <ali909392@gmail.com>
2025-11-13 16:50:54 +0330

    fix: add ignore rule for chore release messages in commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,4 +3,8 @@ module.exports = {
     rules: {
         "header-max-length": [2, "always", 200],
     },
+    ignores: [
+        // chore(release): 🔖 bump version to 1.0.0
+        (message) => /^chore\(release\):/.test(message),
+    ],
 };
